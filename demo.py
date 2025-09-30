383d42269d52b7362d8296eae4ef4b2254ec6442
RUF <rusamentiaga@uniovi.es>
2025-09-30 19:02:13 +0200

    Add demo with skewness, kurtosis, PCT and TSR usage

---
+++ demo.py
@@ -0,0 +1,114 @@
+#%%
+%matplotlib widget
+%load_ext autoreload
+%autoreload 2
+import matplotlib.pyplot as plt
+import seaborn as sns
+import torch
+import numpy as np
+import fastait.io
+import fastait.data
+from fastait.tsr import tsr
+from fastait.pct import pct
+from fastait.ppt import ppt
+from fastait.statistics import skewness, kurtosis
+from fastait.plot import show, plot_image_grid
+from fastait.profiler import TimeProfiler
+
+sns.set_context("paper")
+sns.set_style('whitegrid', {'font.family':'serif', 'font.serif':'Times New Roman'})
+
+from matplotlib.colors import ListedColormap
+colors = np.loadtxt('misc/rain.csv', delimiter=',') / 255.0 
+cmap = ListedColormap(colors)
+
+#%%
+images = fastait.io.load_csv_folder("data/CFRP-006_facq-145Hz_s-Front_Img-2000", 
+                                    dtype=torch.float32, verbose=True)
+print(f"Loaded images tensor shape: {images.shape}, dtype: {images.dtype}")
+roi_margin_top, roi_margin_left = 20, 20
+roi_margin_bottom, roi_margin_right = 40, 20
+images = images[:, roi_margin_top:-roi_margin_bottom, roi_margin_left:-roi_margin_right].contiguous()
+print(f"Cropped images to remove margin: new shape {images.shape}")
+images_normalized = fastait.data.normalize_percentile(images, 3.0, 100.0)
+cooling = fastait.data.extract_cooling(images)
+cooling = cooling.to("cuda")
+
+#%%
+show(images, cmap=cmap)
+
+#%%
+show(images[100], cmap=cmap)
+
+#%%
+plot_image_grid(cooling, cmap="gray", start_index=0, num_images=12, images_per_row=4, 
+                fig_width=8, row_height=2, step=30)
+
+#%%
+mean_per_image = images.mean(dim=(1, 2)).cpu().numpy()
+t = torch.arange(len(mean_per_image)).numpy()
+
+fig, ax = plt.subplots()
+ax.plot(t, mean_per_image, ".-")
+ax.set_xlabel("Frame")
+ax.set_ylabel("Mean pixel value")
+ax.set_title("Mean Pixel Value per Image")
+# log x
+ax.set_xscale('log')
+ax.grid(True)
+fig.tight_layout()
+plt.show()
+
+#%%
+with TimeProfiler("Skewness") as tp:
+    data_skewness = skewness(cooling)
+
+show(data_skewness, cmap=cmap)
+
+#%%
+with TimeProfiler("Kurtosis") as tp:
+    data_kurtosis = kurtosis(cooling)
+
+show(data_kurtosis, cmap=cmap)
+
+#%%
+data = cooling.to(torch.float32)
+with TimeProfiler("PCT") as tp:
+    data_pct = pct(data, 50)
+
+plot_image_grid(data_pct.cpu(), cmap="gray", start_index=0, num_images=8, images_per_row=4, 
+                fig_width=8, row_height=2, step=1)
+
+show(data_pct, cmap=cmap)
+
+#%%
+data = cooling.to(torch.float32)
+with TimeProfiler("TSR") as tp:
+    tsr_res = tsr(data, degree=7)
+    data_tsr = tsr_res.reconstruction()
+    data_der1 = tsr_res.first_derivative()
+    data_der2 = tsr_res.second_derivative()
+
+plot_image_grid(data_der2.cpu(), cmap="gray", start_index=0, num_images=8, images_per_row=4, 
+                fig_width=8, row_height=2, step=10)
+
+show(data_der2, cmap=cmap)
+
+#%%
+data_tsr = tsr_res.reconstruction()
+i = 0
+j = 0
+plt.figure()
+plt.plot(cooling[:, i, j].cpu().numpy(), 'k.', label='Noisy')
+plt.plot(data_tsr[:, i, j].cpu().numpy(), 'r-', label='Fitted')
+plt.title(f'Pixel ({i},{j})')
+plt.legend()
+plt.tight_layout()
+plt.show()
+
+#%%
+data = cooling.to(torch.float32)
+with TimeProfiler("PPT") as tp:
+    data_phase = ppt(data)
+
+show(data_phase, cmap=cmap)
