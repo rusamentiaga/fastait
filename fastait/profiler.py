d831cb37018c112871ec25b9f5f9c2839fb840be
RUF <rusamentiaga@uniovi.es>
2025-09-30 16:44:01 +0200

    Refactor TimeProfiler to add logger support and improve variable naming

--- fastait/profiler.py
+++ fastait/profiler.py
@@ -2,33 +2,31 @@ import time
 import torch
 
 class TimeProfiler:
-    def __init__(self, name=None, cuda_sync=True):
+    def __init__(self, name=None, cuda_sync=True, logger=print):
         self.name = name
-        self.cuda_sync = cuda_sync
-        self.dt = None
-        if not torch.cuda.is_available():
-            self.cuda_sync = False
+        self.dt_ms = None
+        self.logger = logger
+        self.cuda_sync = cuda_sync and torch.cuda.is_available()
 
     def __enter__(self):
         self.start = self.time()
         return self
 
-    def __exit__(self, type, value, traceback):
+    def __exit__(self, exc_type, exc_value, traceback):
         end = self.time()
         elapsed = end - self.start
-        self.dt = elapsed * 1000
+        self.dt_ms = elapsed * 1000
         if self.name is not None:
-            print(f"{self.name}: {self}")
+            self.logger(f"{self.name}: {self}")
+        return False
 
     def elapsed_ms(self):
-        if self.dt is None:
+        if self.dt_ms is None:
             raise RuntimeError("Timer has not finished. Use inside a 'with' block or after __exit__.")
-        return self.dt
+        return self.dt_ms
 
     def __str__(self):
-        if self.dt is None:
-            return "Timer not finished."
-        return f"{self.dt:.2f} ms"
+        return f"{self.elapsed_ms():.2f} ms"
 
     def time(self):
         if self.cuda_sync:
